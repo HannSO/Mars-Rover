7691cb12113b3f145d60bedcf35670d14fb99bc9
Hannah <h.smythosbourne@gmail.com>
2016-04-26 04:11:29 +0100

    Spiked out rover_parser

--- lib/rover_parser.rb
+++ lib/rover_parser.rb
@@ -4,14 +4,16 @@ class RoverParser
     @all_input_string = all_input_string
   end
 
-  def run_commands_on_rovers()
-
+  def run_commands_on_rovers
+    for number_rovers do |x|
+      rover = Rover.new
+    end
   end
 
    private
 
   def commands_organised_by_rover
-    
+    relevent_lines.each_slice(2).to_a
   end
 
   def number_rovers
