80bbece5fa9394fc351454bc66257e1ac5dc238d
Hannah <h.smythosbourne@gmail.com>
2016-05-05 10:03:20 +0100

    refactored controller

--- lib/controller.rb
+++ lib/controller.rb
@@ -14,16 +14,22 @@ class Controller
 
   def initiate_rover_fleet
     all_input_string = STDIN.read
-    grid_parser = grid_parser_klass.new(all_input_string)
-    grid = grid_parser.get_object
-    rovers_parser = multiple_rovers_parser_klass.new(all_input_string, grid)
-    rovers_parser.initiate_rovers_with_commands
-    STDOUT.puts "Output:\n#{rovers_parser.formatted_rovers_results}"
+    set_grid(all_input_string)
+    initiate_rovers(all_input_string)
   end
 
   private
 
-  attr_reader :grid_parser_klass, :multiple_rovers_parser_klass
+  attr_reader :grid_parser_klass, :multiple_rovers_parser_klass, :grid
 
+  def set_grid_object(all_input_string)
+    grid_parser = grid_parser_klass.new(all_input_string)
+    @grid = grid_parser.get_object
+  end
 
+  def initiate_rovers(all_input_string)
+    rovers_parser = multiple_rovers_parser_klass.new(all_input_string, grid)
+    rovers_parser.initiate_rovers_with_commands
+    STDOUT.puts "Output:\n#{rovers_parser.formatted_rovers_results}"
+  end
 end
